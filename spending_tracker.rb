8991d630919fa05869cd0b28d38e88ff01ad40c3
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-08 14:29:42 +0100

    Changed spending_tracker controller and layout so the title reflects the page we're on

--- spending_tracker.rb
+++ spending_tracker.rb
@@ -6,13 +6,16 @@ require( 'sinatra/contrib/all' )
 also_reload( './models/*' )
 
 get '/' do
+  @title = "Go_Go Banking"
   erb( :index )
 end
 
 get '/about' do
+  @title = "GoGo Banking - About"
   erb( :about )
 end
 
 get '/contact' do
+  @title = "GoGo Banking - Contact"
   erb( :contact )
 end
