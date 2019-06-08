0adac2fd02f6c5f0b1bc501167a2622e08244cdc
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-08 11:39:43 +0100

    Core CRUD functionality implemented for merchants

--- models/merchant.rb
+++ models/merchant.rb
@@ -25,9 +25,44 @@ class Merchant
     @id = results.first()['id'].to_i
   end
 
+  def update()
+    sql = "
+    UPDATE merchants
+      SET merchant_name = $1
+      WHERE id = $2"
+    values = [@merchant_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM merchants WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+
+  def self.find(id)
+    sql = "SELECT * FROM merchants WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run( sql, values ).first
+    return Merchant.new( result )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM merchants"
+    transactions = SqlRunner.run( sql )
+    return Merchant.map_all( transactions )
+  end
+
   def self.delete_all()
     sql = "DELETE FROM merchants"
     SqlRunner.run( sql )
   end
+
+  def self.map_all( merchants_data )
+    merchants = merchants_data.map { |merchant| Merchant.new( merchant )}
+    return merchants
+  end
+
 
 end
