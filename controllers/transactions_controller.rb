526515447bc0817f89dda8223e6d188399b3e883
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-08 17:21:21 +0100

    Added running total to home page (still need to sort out pence)

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,4 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry' )
+require_relative( '../models/transaction.rb' )
--- spending_tracker.rb
+++ spending_tracker.rb
@@ -1,12 +1,13 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-# require_relative('./controllers/transactions_controller')
+require_relative('./controllers/transactions_controller')
 # require_relative('./controllers/merchants_controller')
 # require_relative('./controllers/tags_controller')
 also_reload( './models/*' )
 
 get '/' do
   @title = "Go_Go Banking"
+  @current_balance = Transaction.all_transactions_total()/100
   erb( :index )
 end
 
