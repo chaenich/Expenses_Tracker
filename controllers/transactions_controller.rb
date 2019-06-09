ac2965e1d5a416e35c92e136b4363f089314a024
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-09 11:26:58 +0100

    Basic display of all transactions + associated details. Still to sort proper display of currency.

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -2,3 +2,12 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry' )
 require_relative( '../models/transaction.rb' )
+also_reload( '../models/*' )
+
+get '/explore/transactions' do
+  @title = "GoGo Banking - Explore - Transactions"
+  @transactions = Transaction.all_transactions_detail()
+  @total_spent =  Transaction.all_transactions_total()
+binding.pry
+  erb( :"transactions/index" )
+end
