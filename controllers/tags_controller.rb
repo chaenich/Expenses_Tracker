98e594965418ded51dc8e68e8a1a11a9f4d62d6e
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-09 19:49:33 +0100

    Tag create functionality implemented

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -4,13 +4,23 @@ require( 'pry' )
 require_relative( '../models/tag.rb' )
 also_reload( '../models/*' )
 
-get '/manage/tags' do
+get '/manage/tags' do # index
   @title = "GoGo Banking - Manage - Tags"
   @tags = Tag.all()
   erb( :"tags/index" )
 end
 
-post '/manage/tags/:id/delete' do
+get '/manage/tags/new' do # new - new rec part1
+  erb( :"tags/new" )
+end
+
+post '/manage/tags' do # create - new rec part2
+  @tag = Tag.new( params )
+  @tag.save()
+  redirect to '/manage/tags'
+end
+
+post '/manage/tags/:id/delete' do # delete / destroy
   tag = Tag.find( params[:id] )
   tag.delete()
   redirect to '/manage/tags'
