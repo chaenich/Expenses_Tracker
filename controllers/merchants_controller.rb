3c2903c4c370d1bbc63be7f197425171a05cc1c4
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-09 19:57:32 +0100

    Merchant create functionality implemented

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -4,13 +4,23 @@ require( 'pry' )
 require_relative( '../models/merchant.rb' )
 also_reload( '../models/*' )
 
-get '/manage/merchants' do
+get '/manage/merchants' do # index
   @title = "GoGo Banking - Manage - Merchants"
   @merchants = Merchant.all()
   erb( :"merchants/index" )
 end
 
-post '/manage/merchants/:id/delete' do
+get '/manage/merchants/new' do # new - new rec part1
+  erb( :"merchants/new" )
+end
+
+post '/manage/merchants' do # create - new rec part2
+  @merchant = Merchant.new( params )
+  @merchant.save()
+  redirect to '/manage/merchants'
+end
+
+post '/manage/merchants/:id/delete' do # delete / destroy
   merchant = Merchant.find( params[:id] )
   merchant.delete()
   redirect to '/manage/merchants'
