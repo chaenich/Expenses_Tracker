4aa46c6688ece8d17f86712bb5c9d3f762b6e7dd
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-09 19:00:55 +0100

    Merchant delete functionality implemented

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -9,3 +9,9 @@ get '/manage/merchants' do
   @merchants = Merchant.all()
   erb( :"merchants/index" )
 end
+
+post '/manage/merchants/:id/delete' do
+  merchant = Merchant.find( params[:id] )
+  merchant.delete()
+  redirect to '/manage/merchants'
+end
