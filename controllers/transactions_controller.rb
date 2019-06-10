9fdbf8125177e739cac3db140a9218b39d79a7df
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-10 08:22:39 +0100

    Transaction delete functionality implemented

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -8,5 +8,17 @@ get '/explore/transactions' do
   @title = "GoGo Banking - Explore - Transactions"
   @transactions = Transaction.all_transactions_detail()
   @total_spent =  Transaction.all_transactions_total()
-  erb( :"transactions/index" )
+  erb( :"transactions/index_explore" )
+end
+
+get '/manage/transactions' do
+  @title = "GoGo Banking - Manage - Transactions"
+  @transactions = Transaction.all_transactions_detail()
+  erb( :"transactions/index_manage" )
+end
+
+post '/manage/transactions/:id/delete' do # delete / destroy
+  transaction = Transaction.find( params[:id] )
+  transaction.delete()
+  redirect to '/manage/transactions'
 end
